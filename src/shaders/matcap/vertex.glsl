ec1857ddbb63a971c4fe963d3a129549e49975ce
luosijie <luosijie@jsunet.cn>
2023-05-11 17:08:24 +0800

    fix: matcap shader

--- src/shaders/matcap/vertex.glsl
+++ src/shaders/matcap/vertex.glsl
@@ -1,11 +1,20 @@
 varying vec2 vPoint;
 
 void main () {
+    vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+
+    gl_Position = projectionMatrix * modelViewPosition;
+
+
+    // Set matcap coordinate
     vec3 n = mat3(modelViewMatrix) * normal;
     n = normalize(n);
 
-    vPoint.x = n.x * .5 + .5;
-    vPoint.y = n.y * .5 + .5;
+    vec3 normalizedPosition = normalize(modelViewPosition.xyz);
+    vec3 x = normalize(vec3(normalizedPosition.z, 0., normalizedPosition.x));
+    vec3 y = cross(normalizedPosition, x);
+    vec2 point = vec2(dot(x, n), dot(y, n));
+    point = point * .5 + .5; 
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vPoint = point;
 }
