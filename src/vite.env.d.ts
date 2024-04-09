60bdf356f2b72e3905b759a77d61bee5965c1807
luosijie <luo_sj@163.com>
2024-04-09 15:40:54 +0800

    update

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -1,3 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_SUB_DOMAIN: string
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
 declare module '*glsl' {
     const content: string
     export default content
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import * as path from 'path'
 
 import glsl from 'vite-plugin-glsl'
 
-export default defineConfig({
-    server: {
-        port: 3333
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    plugins: [
-        glsl({ watch: true })
-    ]
+export default defineConfig(({ mode }) => {
+    const root = process.cwd()
+    const env = loadEnv(mode, root)
+
+    return {
+        base:  env.VITE_SUB_DOMAIN || '/',
+        server: {
+            port: 3333
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        },
+        plugins: [
+            glsl({ watch: true })
+        ]
+    }
 })
